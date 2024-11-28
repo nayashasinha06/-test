72744db2b514e968e40148b79f1feeb17a8c526b
nayshasin06 <nayashasin@gmail.com>
2024-11-28 15:34:00 +0530

    test commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express ( ) ;
+
+app.get("/", (req, res) => {
+ res.send("Hello World");
+});
+app.get("/date", (req, res) => {
+ res.send(new Date().toString());
+});
+app.use((req, res) => {
+ res.status(404).send("Page not found");
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+ console.log(`Server is running on port http://localhost:${PORT}` );
+});
